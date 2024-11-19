255052ebab5c8ccf7ddcc80b89694a0ded7ce4a9
PaulineDuarte <PaulineDuarte1@hotmail.com>
2024-11-19 12:53:55 -0300

    Update Button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+
+    background-color: #fafafa; 
+    border: 1px solid #fafafa;
+    border-radius: 28px; 
+
+
+    height: 62px; 
+    width:80%;
+
+
+    margin:20px; 
+
+    &: hover {
+    background-color:#FAFAFA40; 
+    cursor: pointer; 
+    }
+`
